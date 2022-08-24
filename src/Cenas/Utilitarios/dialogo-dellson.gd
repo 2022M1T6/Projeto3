22372c3925fc36a02576b15cc611e69a638b9dfd
TechioRafael <110608373+RafaelTechio@users.noreply.github.com>
2022-08-24 14:03:32 -0300

    FEAT: Primeira sequência de falas completa

--- src/Cenas/Utilitarios/dialogo-dellson.gd
+++ src/Cenas/Utilitarios/dialogo-dellson.gd
@@ -1,47 +1,101 @@
 extends PopupDialog
 
-onready var label = $TextureRect/Label
+onready var dialogBoxLabel = $DialogBox/Label
 var podeIrParaProximaLinha = false
-var dialogos = []
+var completarFrase = false
 
+var dialogos = []
+var dialogoAtual = {"personagem": "", "falas": []}
+
+
+func getDialogSpriteFilePath():
+	if dialogoAtual["personagem"] == 'dellson':
+		return "res://Public/dellsonCaixaDialogo.png"
+	elif dialogoAtual["personagem"] == 'jose':
+		return "res://Public/Jose.png"
+	
+	return "res://Public/icon.png"
+		
+# Função que mostra a próxima mensagem do array de diálogos
 func mostrarMensagem() -> void:
-	if dialogos.size() == 0:
+	if dialogoAtual["falas"].size() == 0 && dialogos.size() == 0:
 		get_tree().paused = false
 		hide()
 		return
 	
-	var text = dialogos.pop_front()
-	label.percent_visible = 0
-	label.text = text
+	if(dialogoAtual["falas"].size() == 0):
+		dialogoAtual = dialogos.pop_front()
+	
+	var text = dialogoAtual["falas"].pop_front()	
+	dialogBoxLabel.percent_visible = 0
+	dialogBoxLabel.text = text
 	podeIrParaProximaLinha = false
+
+	# Verifica se a foto da caixa de diálogo está condizente com o personagem e troca caso necessário
+	$DialogSprite.texture = load(getDialogSpriteFilePath())
+
 	show()
 	return
 	
 func _process(delta):
-	if podeIrParaProximaLinha && Input.is_action_just_pressed("interact"):
-		mostrarMensagem()
+	if Input.is_action_just_pressed("interact"):
+		if(podeIrParaProximaLinha):
+			mostrarMensagem()
+		else:
+			completarFrase = true
 
 func _on_Timer_timeout():
-	if(label.percent_visible >= 1):
+	if(dialogBoxLabel.percent_visible >= 1):
 		podeIrParaProximaLinha = true
 	else:
-		var percentVisible = 0.01
-		if(len(label.text) < 30):
-			percentVisible = 0.03
-		elif len(label.text) >= 30 && len(label.text) <= 60:
-			percentVisible = 0.02
-			
-		label.percent_visible += percentVisible
+		if completarFrase:
+			dialogBoxLabel.percent_visible = 1
+			podeIrParaProximaLinha = true
+			completarFrase = false
+		else:
+			var percentVisible = 0.01
+			if(len(dialogBoxLabel.text) < 30):
+				percentVisible = 0.03
+			elif len(dialogBoxLabel.text) >= 30 && len(dialogBoxLabel.text) <= 60:
+				percentVisible = 0.02
+				
+			dialogBoxLabel.percent_visible += percentVisible
 
 
 func _on_Area2D_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	dialogos = [
-		'Ei, você!',
-		'Você mesmo com cara de bobão!',
-		'Preciso falar com a Fabi para ter pelo menos um tutorial de como se mexer antes dos novatos chegarem aqui…',
-		'Mas ainda bem que você tem…',
-		'EEEU DELLSON! Seu capitão! Ou comandante!'
+		{
+			'personagem': 'jose',
+			'falas': [
+				'Uooou. Esse treinamento é REALMENTE imersivo.'
+			]
+		},
+		{
+			'personagem': 'dellson',
+			'falas': [
+				'Ei, você!',
+				'Você mesmo com cara de bobão!',
+				'Preciso falar com a Fabi para ter pelo menos um tutorial de como se mexer antes dos novatos chegarem aqui…',
+				'Mas ainda bem que você tem…',
+				'EEEU DELLSON! Seu capitão! Ou comandante!'
+			]
+		},
+		{
+			'personagem': 'fabiana',
+			'falas': [
+				'Instrutor, Dellson. Instrutor.'
+			]
+		},
+		{
+			'personagem': 'dellson',
+			'falas': [
+				'Pode ser, Fabi, pode ser…',
+				'Vem cá, novato. EU serei seu mestre dos magos',
+				'*Pausa',
+				'Ah é, você se movimenta com WASD',
+				'Venha até aqui para saber mais.'
+			]
+		}
 	]
-	
-	get_tree().paused = true
 	mostrarMensagem()
+	get_tree().paused = true
