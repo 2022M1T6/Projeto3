36151b06909a10ff4c803600a6470375f3b6f135
TechioRafael <110608373+RafaelTechio@users.noreply.github.com>
2022-08-24 11:13:56 -0300

    FIX: correção do teleporte entre mapas e diálogo

--- src/Cenas/Utilitarios/dialogo-dellson.gd
+++ src/Cenas/Utilitarios/dialogo-dellson.gd
@@ -1,27 +1,40 @@
 extends PopupDialog
 
-onready var label = $"Barra-de-dialogo-dellson/Label"
-
+onready var label = $TextureRect/Label
+var podeIrParaProximaLinha = false
 var dialogos = []
 
 func mostrarMensagem() -> void:
 	if dialogos.size() == 0:
+		get_tree().paused = false
 		hide()
 		return
 	
 	var text = dialogos.pop_front()
-	label.visible_characters = 0
+	label.percent_visible = 0
 	label.text = text
-	print('to mostrando')
+	podeIrParaProximaLinha = false
 	show()
 	return
 	
+func _process(delta):
+	if podeIrParaProximaLinha && Input.is_action_just_pressed("interact"):
+		mostrarMensagem()
 
 func _on_Timer_timeout():
-	label.visible_characters += 1
+	if(label.percent_visible >= 1):
+		podeIrParaProximaLinha = true
+	else:
+		var percentVisible = 0.01
+		if(len(label.text) < 30):
+			percentVisible = 0.03
+		elif len(label.text) >= 30 && len(label.text) <= 60:
+			percentVisible = 0.02
+			
+		label.percent_visible += percentVisible
 
 
-func _on_Area2D_area_entered(area):
+func _on_Area2D_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	dialogos = [
 		'Ei, você!',
 		'Você mesmo com cara de bobão!',
@@ -29,5 +42,6 @@ func _on_Area2D_area_entered(area):
 		'Mas ainda bem que você tem…',
 		'EEEU DELLSON! Seu capitão! Ou comandante!'
 	]
-	print('oi')
+	
+	get_tree().paused = true
 	mostrarMensagem()
