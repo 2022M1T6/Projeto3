a41970ec09b65572642e174e5c5e8223a6c7e668
gabriel-gmc <gabriel.coutinho@sou.inteli.edu.br>
2022-09-19 14:05:09 -0300

    Scene 2 bug fix

--- src/Cenas/Fases/Tutorial/Mapa2/Labirinto.gd
+++ src/Cenas/Fases/Tutorial/Mapa2/Labirinto.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var first = false
 # Vai para a próxima área
 func _on_Area2D_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	get_tree().change_scene("res://Cenas/Fases/Tutorial/Mapa3/Combate.tscn")
@@ -22,13 +23,16 @@ func _on_TutorialMapa2Fala1_area_shape_exited(area_rid, area, area_shape_index, 
 
 
 func _on_TuroalMapa2Fala2_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	$Jose/Camera2D/CanvasLayer/PopupDialog.sendDialog([
-			{
-				'personagem': 'dellson',
-				'falas': [
-					'BOO!!',
-					'Errei o timing?',
-					'… Enfim, quando você se deparar com um lugar o qual não consegue passar, pressione ESPAÇO para atravessar pequenos obstáculos.'
-				]
-			}
-		])
+	if first == false:
+		$Jose/Camera2D/CanvasLayer/PopupDialog.sendDialog([
+				{
+					'personagem': 'dellson',
+					'falas': [
+						'BOO!!',
+						'Errei o timing?',
+						'… Enfim, quando você se deparar com um lugar o qual não consegue passar, pressione ESPAÇO para atravessar pequenos obstáculos.'
+					]
+				}
+			])
+		first = true
+	
