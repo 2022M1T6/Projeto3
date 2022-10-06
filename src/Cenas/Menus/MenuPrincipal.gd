416b966bab58bd9e338d8b11cb53f4588a96335c
TechioRafael <110608373+RafaelTechio@users.noreply.github.com>
2022-10-06 01:42:47 -0300

    BUILD: botão prólogo e build

--- src/Cenas/Menus/MenuPrincipal.gd
+++ src/Cenas/Menus/MenuPrincipal.gd
@@ -11,6 +11,14 @@ func _on_BotaoOpcoes_pressed():
 # Função executada quando o botão Sair é pressionado
 func _on_BotaoSair_pressed():
 	return get_tree().quit()
+	
+# Função executada quando o botão prólogo é pressionado. Abre um vídeo no youtube com o prólogo
+func _on_BotaoPrologo_pressed():
+	if GlobalOptions.isPortuguese:
+		OS.shell_open("https://youtu.be/RwOVy2hh93U")
+	else:
+		OS.shell_open("https://youtu.be/JMVG3NpSRYo")
+
 
 # Troca os textos de acordo com a linguagem
 func _ready():
@@ -19,11 +27,14 @@ func _ready():
 		$VBoxContainer/BotaoNovoJogo/Label.text = 'Novo Jogo'
 		$VBoxContainer/BotaoOpcoes/Label.text = 'Opções'
 		$VBoxContainer/BotaoSair/Label.text = 'Sair'
+		$VBoxContainer/BotaoPrologo/Label.text = 'Prólogo'		
 	else:
 		$VBoxContainer/BotaoNovoJogo/Label.text = 'New Game'
 		$VBoxContainer/BotaoOpcoes/Label.text = 'Options'
 		$VBoxContainer/BotaoSair/Label.text = 'Quit'
+		$VBoxContainer/BotaoPrologo/Label.text = 'Prologue'				
 		
 func _process(delta):
 	$AudioStreamPlayer2D.volume_db = GlobalOptions.setMusicSound(-5)
 		
+
